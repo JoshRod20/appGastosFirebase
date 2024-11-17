c4a26b950760f09d0d4658d6b1d7455c84825182
KennRod <rodriguezjosh2003@gmail.com>
2024-11-16 21:48:07 -0600

    Cargando el arch

---
+++ appGastos/src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbUtn9GZLZzcYBPEaU3K7W7auPTmpK7e0",
+  authDomain: "appgastosfirebase-a6fa7.firebaseapp.com",
+  projectId: "appgastosfirebase-a6fa7",
+  storageBucket: "appgastosfirebase-a6fa7.firebasestorage.app",
+  messagingSenderId: "673242528798",
+  appId: "1:673242528798:web:c501a93355cda00571c20f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
